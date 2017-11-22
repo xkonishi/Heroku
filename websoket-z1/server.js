baf9f7401ac65b5aee2c0634a9f201d835c046a1
xkonishi <konishi.cnb@gmail.com>
2017-11-23 02:21:34 +0900

    z

---
+++ websoket-z1/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', (message) => {
+	  ws.send(message);
+  });
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
